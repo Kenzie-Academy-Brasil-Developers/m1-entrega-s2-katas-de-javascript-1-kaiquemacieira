8ae941a637e2f7b7688bee6b875c5ad8e8f399f0
kaique <kaique@kaiqs6669>
2021-10-25 10:22:27 -0300

    Katas_1 finalizado

--- katas1.js
+++ katas1.js
@@ -1,94 +1,116 @@
-function oneThroughTwenty() {
-    
-   /* Your code goes below
-   Write a for or a while loop
-   return the result*/
-    
+
+//1 a 20
+function oneThroughTwenty(){
+  let value = [];
+      for(let i = 1; i <= 20; i++){
+          value.push(i);
+      }
+  return value
+  }
+
+
+  //pares
+function evensToTwenty(){
+  let pares = [];
+  for(let i = 1; i <= 20; i++){
+      if (i % 2 === 0){
+          pares.push(i);
+      }
+  }
+  return pares
 }
 
-//call function oneThroughTwenty
+//Impares
+function oddsToTwenty(){
+  let impares = [];
+  for (let i = 1; i <= 20; i++){
+      if (i % 2 !== 0){
+          impares.push(i);
+      }
+  }
+  return impares;
+} 
 
-function evensToTwenty() {
-    
-   /* Your code goes below
-   Write a for or a while loop
-   return the result */
-  
+//Multiplos de 5
+
+function multiplesOfFive(){
+  let multiplos = [];
+  for (let i = 5; i <= 100; i++){
+      if (i % 5 === 0){
+          multiplos.push(i);
+      }
+  }
+  return multiplos;
 }
 
-//call function evensToTwenty
+//Quadrados perfeitos
 
-function oddsToTwenty() {
-    
-  /* Your code goes below
-  Write a for or a while loop
-  return the result */
-   
+function squareNumbers(){
+  let resultado = [];
+  let quadrado = 0;
+      for (let i = 0; i < 100; i++){
+      quadrado = i * i;
+          if (quadrado > 0 && quadrado <= 100){
+              resultado.push(quadrado);
+          }
+      }
+  return resultado
+}
+//Numeros contando de trás pra frente
+
+function countingBackwards(){
+  let backin = [];
+  for (let i = 20; i >= 1; i--){
+      backin.push(i);       
+  }
+  return backin
 }
 
-//call function oddsToTwenty
+//Numeros pares de 20 até 1
 
-function multiplesOfFive() {
-    
-  /* Your code goes below
-  Write a for or a while loop
-  return the result */
+function evenNumbersBackwards(){
+  let backPar = [];
+  for (let i = 20; i >= 1; i--){
+      if (i % 2 === 0){
+          cereja.push(i);
+      }
 
+  }
+  return backPar
 }
 
-//call function multiplesOfFive
+//ímpares de 20 até 1
 
-function squareNumbers() {
-    
- /* Your code goes below
-  Write a for or a while loop
-  return the result */
-    
+function oddNumbersBackwards(){
+  let backImpar = [];
+  for (i = 20; i >= 1; i--){
+      if(i % 2 !== 0){
+          uva.push(i);
+      }
+  }
+  return backImpar}
+
+//Multiplos de 5 de trás pra frente
+
+function multiplesOfFiveBackwards(){
+  let backMultiply = [];
+  for (let i = 100; i >= 5; i--){
+      if (i % 5 === 0){
+          backMultiply.push(i);           
+      }
+  }
+  return backMultiply
 }
 
-//call function squareNumbers
+// quadrados perfeitos de trás pra frente a partir de 100
 
-function countingBackwards() {
-    
-  /* Your code goes below
-  Write a for or a while loop
-  return the result */
+function squareNumbersBackwards(){
+  let resultado = [];
+      for (let i = 100; i > 0; i--){
+      quadrado = i * i;
+          if (quadrado <= 100 && quadrado > 0){
+              resultado.push(quadrado);
+          }
+      }
+  return resultado
 }
-
-//call function countingBackwards
-
-function evenNumbersBackwards() {
-    
- /* Your code goes below
-  Write a for or a while loop
-  return the result */
-}
-
-//call function evenNumbersBackwards
-
-function oddNumbersBackwards() {
-    
- /* Your code goes below
-  Write a for or a while loop
-  return the result */
-}
-
-//call function oddNumbersBackwards
-
-function multiplesOfFiveBackwards() {
-    
- /* Your code goes below
-  Write a for or a while loop
-  return the result */
-}
-
-//call function multiplesOfFiveBackwards
-
-function squareNumbersBackwards() {
-    
-   /* Your code goes below
-  Write a for or a while loop
-  return the result */
-}
-
-//call function squareNumbersBackwards
